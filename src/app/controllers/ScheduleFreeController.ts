531c24d76a5552fe0d3e223067cf2a333f393172
Rich Ramalho <richelton14@gmail.com>
2022-03-07 14:32:56 -0300

    fix bug: error handler

--- src/app/controllers/ScheduleFreeController.ts
+++ src/app/controllers/ScheduleFreeController.ts
@@ -18,67 +18,71 @@ class ScheduleFreeController {
     private readonly HOUR_END = '19:00';
 
     async index(req: Request, res: Response) {
-        const { schedule, date_start } = req.query as { [key: string]: string };
-        const dt_moment = moment(new Date(date_start), 'YYYY-MM-DD').utcOffset("+0000");
+        try {
+            const { schedule, date_start } = req.query as { [key: string]: string };
+            const dt_moment = moment(new Date(date_start), 'YYYY-MM-DD').utcOffset("+0000");
 
-        const schedulesDate = await this.scheduleFreeService.find({
-            where: {
-                schedule_id: schedule,
-                date_hour_start: {
-                    [Op.between]: [
-                        dt_moment.startOf('day').toDate(),
-                        dt_moment.endOf('day').toDate()
-                    ]
-                }
-            },
-            order: [['date_hour_start', 'ASC']]
-        });
-        const schedulesBusy: ISchedule[] = schedulesDate.map(schBusy => {
-            return {
-                from:  moment(schBusy.date_hour_start).utcOffset("+0000").format('HH:mm'),
-                of: moment(schBusy.date_hour_end).utcOffset("+0000").format('HH:mm'),
-                status: 'OCUPADO'
-            }
-        });
-
-        const schedules: ISchedule[] = [...schedulesBusy];
-        if (schedules.length === 0) {
-            schedules.push({
-                from: this.HOUR_START,
-                of: this.HOUR_END,
-                status: "LIVRE"
+            const schedulesDate = await this.scheduleFreeService.find({
+                where: {
+                    schedule_id: schedule,
+                    date_hour_start: {
+                        [Op.between]: [
+                            dt_moment.startOf('day').toDate(),
+                            dt_moment.endOf('day').toDate()
+                        ]
+                    }
+                },
+                order: [['date_hour_start', 'ASC']]
             });
-        }
-        let schAux = '';
-        schedulesBusy.forEach((schedule, idx) => {
-            if (idx === 0 && schedule.from !== this.HOUR_START) {
+            const schedulesBusy: ISchedule[] = schedulesDate.map(schBusy => {
+                return {
+                    from:  moment(schBusy.date_hour_start).utcOffset("+0000").format('HH:mm'),
+                    of: moment(schBusy.date_hour_end).utcOffset("+0000").format('HH:mm'),
+                    status: 'OCUPADO'
+                }
+            });
+
+            const schedules: ISchedule[] = [...schedulesBusy];
+            if (schedules.length === 0) {
                 schedules.push({
                     from: this.HOUR_START,
-                    of: schedule.from,
-                    status: "LIVRE"
-                });
-            }
-
-            if (schAux !== '' && schedule.from !== schAux) {
-                schedules.push({
-                    from: schAux,
-                    of: schedule.from,
-                    status: "LIVRE"
-                });
-            }
-            schAux = schedule.of;
-
-            if (idx === schedulesBusy.length - 1 && schedule.of !== this.HOUR_END) {
-                schedules.push({
-                    from: schAux,
                     of: this.HOUR_END,
                     status: "LIVRE"
                 });
             }
-        });
-        schedules.sort((a, b) => a.from.localeCompare(b.from));
+            let schAux = '';
+            schedulesBusy.forEach((schedule, idx) => {
+                if (idx === 0 && schedule.from !== this.HOUR_START) {
+                    schedules.push({
+                        from: this.HOUR_START,
+                        of: schedule.from,
+                        status: "LIVRE"
+                    });
+                }
 
-        return res.status(200).json(schedules);
+                if (schAux !== '' && schedule.from !== schAux) {
+                    schedules.push({
+                        from: schAux,
+                        of: schedule.from,
+                        status: "LIVRE"
+                    });
+                }
+                schAux = schedule.of;
+
+                if (idx === schedulesBusy.length - 1 && schedule.of !== this.HOUR_END) {
+                    schedules.push({
+                        from: schAux,
+                        of: this.HOUR_END,
+                        status: "LIVRE"
+                    });
+                }
+            });
+            schedules.sort((a, b) => a.from.localeCompare(b.from));
+
+            return res.status(200).json(schedules);
+        } catch (error) {
+            return res.status(400).json({ message: 'Algum erro ocorreu!' });
+        }
     }
 }
 
