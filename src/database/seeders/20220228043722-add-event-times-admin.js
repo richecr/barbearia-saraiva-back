d7fecdafdb499920d3e0344d872b63fae989b5f3
Rich Ramalho <richelton14@gmail.com>
2022-03-20 13:21:34 -0300

    updated seed of events types

--- src/database/seeders/20220228043722-add-event-times-admin.js
+++ src/database/seeders/20220228043722-add-event-times-admin.js
@@ -5,45 +5,113 @@ module.exports = {
             [
                 {
                     id: 1,
-                    event_name: "corte",
-                    duration: 60,
+                    event_name: 'corte',
+                    duration: 45,
                     created_at: new Date(),
                     updated_at: new Date(),
                 },
                 {
                     id: 2,
-                    event_name: "barba",
-                    duration: 30,
+                    event_name: 'barba',
+                    duration: 20,
                     created_at: new Date(),
                     updated_at: new Date(),
-                },{
+                },
+                {
                     id: 3,
-                    event_name: "corte+barba",
-                    duration: 90,
+                    event_name: 'barboterapia',
+                    duration: 25,
                     created_at: new Date(),
                     updated_at: new Date(),
-                },{
+                },
+                {
                     id: 4,
-                    event_name: "corte+hidratacao",
-                    duration: 60,
+                    event_name: 'sobrancelha',
+                    duration: 10,
                     created_at: new Date(),
                     updated_at: new Date(),
-                },{
+                },
+                {
                     id: 5,
-                    event_name: "botox",
+                    event_name: 'hidratação',
+                    duration: 15,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 6,
+                    event_name: 'acabamento',
+                    duration: 10,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 7,
+                    event_name: 'progressiva',
+                    duration: 100,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 8,
+                    event_name: 'botox',
+                    duration: 100,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 9,
+                    event_name: 'tintura',
                     duration: 60,
                     created_at: new Date(),
                     updated_at: new Date(),
-                },{
-                    id: 6,
-                    event_name: "progressiva",
-                    duration: 90,
+                },
+                {
+                    id: 10,
+                    event_name: 'platinado',
+                    duration: 60,
                     created_at: new Date(),
                     updated_at: new Date(),
-                },{
-                    id: 7,
-                    event_name: "corte+barba+progressiva",
-                    duration: 120,
+                },
+                {
+                    id: 11,
+                    event_name: 'corte+barba',
+                    duration: 60,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 12,
+                    event_name: 'corte+barba+sobrancelha',
+                    duration: 70,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 13,
+                    event_name: 'corte+barba+hidratacao',
+                    duration: 75,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 14,
+                    event_name: 'corte+barba+progressiva',
+                    duration: 110,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 15,
+                    event_name: 'corte+barba+botox',
+                    duration: 110,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                },
+                {
+                    id: 16,
+                    event_name: 'corte+barboterapia',
+                    duration: 70,
                     created_at: new Date(),
                     updated_at: new Date(),
                 },
@@ -55,7 +123,13 @@ module.exports = {
         const Op = Sequelize.Op;
         return queryInterface.bulkDelete(
             'EventTimes',
-            { id: { [Op.in]: [1,2,3,4,5,6,7] } },
+            {
+                id: {
+                    [Op.in]: [
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                    ],
+                },
+            },
             {},
         );
     },
